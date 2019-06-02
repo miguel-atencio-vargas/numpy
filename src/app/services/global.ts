7b358d421b9e853211cefd2832449dc2de563f70
skyfall947 <atencio947@gmail.com>
2019-06-02 19:43:29 -0400

    debugg

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,8 +1,8 @@
-/* export let GLOBAL = {
+ export let GLOBAL = {
     url: 'https://agile-crag-29584.herokuapp.com/'
-};  */
+}; 
 
-  export let GLOBAL = {
+/*   export let GLOBAL = {
     url: 'http://localhost:5000/'
 };
- 
+  */
