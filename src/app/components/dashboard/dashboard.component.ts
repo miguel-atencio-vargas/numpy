47fee6975be81ca9d4119f254807c12a19638ef4
skyfall947 <atencio947@gmail.com>
2019-06-02 19:18:15 -0400

    version lastest

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 
 
 export class DashboardComponent implements OnInit {
-  public datos = [];
+  public a = ['Suma de Matrices', 'Resta de Matrices', 'Multiplicacion vectorial de Matrices', 'Multiplicacion escalar de Matrices'];
+  public b = ['Determinante', 'Rango', 'Norma', 'Traspuesta', 'Diagonal', 'Inversa', 'Triangular Inferior', 'Triangular Superior'];
+  public c = ['Numeros aleatorios enteros', 'Numeros aleatorios con punto decimal', 'Numeros aleatorios según una distribucion de probabilidad'];
   constructor() { }
 
   ngOnInit() {
--- src/app/components/export/export.component.ts
+++ src/app/components/export/export.component.ts
@@ -8,31 +8,36 @@ import { OperacionesService } from '../../services/operaciones.service'
   providers: [OperacionesService]
 })
 export class ExportComponent implements OnInit {
-  public rows ;
-  public cols ;
+  public rows = 0; cols = 0;
+  public a = 0; b = 0;
+  public mu: number; sigma: number;
+  public types = ['Integer', 'float', 'normal', 'normal_Scipy', 'Multivariate_normal'];
+  public type;
 
   constructor(private _operacionesService: OperacionesService) { }
 
   ngOnInit() {
   }
-  onExport(json) {
+  onExport(json, name) {
     /* generate worksheet */
-    console.log(json)
-    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(json);
+    console.log(name);
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(json.data);
 
     /* generate workbook and add the worksheet */
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    XLSX.utils.book_append_sheet(wb, ws);
 
     /* save to file */
-    XLSX.writeFile(wb, 'SheetJS.xlsx');
+    XLSX.writeFile(wb, name);
   }
   onSubmit() {
-    const data = {m: this.rows, n: this.cols};
+    const data = {m: this.rows, n: this.cols, a: this.a, b: this.b, type: this.type, mu: this.mu, sigma: this.sigma};
     this._operacionesService.random(data).subscribe(
       response => {
-        response = JSON.parse(response.data).data;
-        this.onExport(response);
+        console.log(response);
+        const name = response.name + '.xlsx';
+        response = JSON.parse(response.data);
+        this.onExport(response, name);
       }
     );
   }
